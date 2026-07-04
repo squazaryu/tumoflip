be6240b0204abb012bc135f89f1d3397345c0ecc
squazaryu <tumowuh@internet.ru>
2026-07-04 09:50:05 +0300

    feat(module-one): add cockpit icon

--- fbt_options.py
+++ fbt_options.py
@@ -16,7 +16,7 @@ DEBUG = 1
 
 # Suffix to add to files when building distribution
 # If OS environment has DIST_SUFFIX set, it will be used instead
-DIST_SUFFIX = "t-dev-089-036-008"
+DIST_SUFFIX = "t-dev-089-036-009"
 
 # Post-update slideshow shown by the updater package
 UPDATE_SPLASH = "tumoflip_update"
