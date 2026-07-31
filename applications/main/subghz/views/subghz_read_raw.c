1c3e41df692706d6f855d4f4ba76c3ebd30020b1
squazaryu <tumowuh@internet.ru>
2026-07-31 23:13:20 +0300

    fix(subghz): separate RAW filename from signal graph

--- applications/main/subghz/views/subghz_read_raw.c
+++ applications/main/subghz/views/subghz_read_raw.c
@@ -5,10 +5,15 @@
 #include <furi_hal.h>
 #include <input/input.h>
 #include <gui/elements.h>
+#include <gui/icon.h>
+#include <string.h>
 
 #include <assets_icons.h>
 #define SUBGHZ_READ_RAW_RSSI_HISTORY_SIZE 100
 #define TAG                               "SubGhzReadRaw"
+#define SUBGHZ_RAW_SAVED_NAME_WIDTH       96U
+#define SUBGHZ_RAW_SAVED_PRESET_WIDTH     37U
+#define SUBGHZ_RAW_SAVED_TEXT_BUFFER      32U
 
 struct SubGhzReadRAW {
     View* view;
@@ -288,15 +293,134 @@ void subghz_read_raw_draw_threshold_rssi(Canvas* canvas, SubGhzReadRAWModel* mod
     canvas_draw_dot(canvas, x - 2, y);
 }
 
-void subghz_read_raw_draw(Canvas* canvas, SubGhzReadRAWModel* model) {
-    uint8_t graphics_mode = 1;
-    canvas_set_color(canvas, ColorBlack);
+static void subghz_read_raw_fit_text(
+    Canvas* canvas,
+    char* display_text,
+    size_t display_text_size,
+    const char* source_text,
+    uint16_t max_width) {
+    bool truncated = strlcpy(display_text, source_text, display_text_size) >= display_text_size;
+
+    while(display_text[0] && (canvas_string_width(canvas, display_text) > max_width)) {
+        display_text[strlen(display_text) - 1U] = '\0';
+        truncated = true;
+    }
+
+    if(truncated) {
+        const uint16_t ellipsis_width = canvas_string_width(canvas, "...");
+        while(display_text[0] &&
+              (canvas_string_width(canvas, display_text) + ellipsis_width > max_width)) {
+            display_text[strlen(display_text) - 1U] = '\0';
+        }
+        strlcat(display_text, "...", display_text_size);
+    }
+}
+
+static void subghz_read_raw_draw_saved_header(Canvas* canvas, SubGhzReadRAWModel* model) {
+    char display_name[SUBGHZ_RAW_SAVED_TEXT_BUFFER];
+
+    canvas_set_font(canvas, FontPrimary);
+    subghz_read_raw_fit_text(
+        canvas,
+        display_name,
+        sizeof(display_name),
+        furi_string_get_cstr(model->file_name),
+        SUBGHZ_RAW_SAVED_NAME_WIDTH);
+    canvas_draw_str(canvas, 2, 10, display_name);
+
+    canvas_draw_rbox(canvas, 103, 0, 25, 12, 2);
+    canvas_invert_color(canvas);
+    canvas_set_font(canvas, FontSecondary);
+    canvas_draw_str(canvas, 108, 8, "RAW");
+    canvas_invert_color(canvas);
+
+    canvas_draw_line(canvas, 0, 14, 127, 14);
+}
+
+static void subghz_read_raw_draw_saved_details(Canvas* canvas, SubGhzReadRAWModel* model) {
+    canvas_set_font(canvas, FontSecondary);
+    canvas_draw_str(canvas, 2, 26, "FREQ");
+
+    canvas_set_font(canvas, FontPrimary);
+    canvas_draw_str(canvas, 31, 27, furi_string_get_cstr(model->frequency_str));
+    const uint16_t frequency_end =
+        31U + canvas_string_width(canvas, furi_string_get_cstr(model->frequency_str));
+
+    canvas_set_font(canvas, FontSecondary);
+    canvas_draw_str(canvas, frequency_end + 2U, 26, "MHz");
+    canvas_draw_line(canvas, 2, 32, 126, 32);
+
+    canvas_draw_str(canvas, 2, 44, "MOD");
+    canvas_set_font(canvas, FontPrimary);
+    char display_preset[SUBGHZ_RAW_SAVED_TEXT_BUFFER];
+    subghz_read_raw_fit_text(
+        canvas,
+        display_preset,
+        sizeof(display_preset),
+        furi_string_get_cstr(model->preset_str),
+        SUBGHZ_RAW_SAVED_PRESET_WIDTH);
+    canvas_draw_str(canvas, 31, 45, display_preset);
+
+    canvas_set_font(canvas, FontSecondary);
+    canvas_draw_str(canvas, 71, 44, "RADIO");
+    canvas_draw_rbox(canvas, 104, 34, 24, 13, 2);
+    canvas_invert_color(canvas);
+    canvas_draw_str(
+        canvas, 109, 43, (model->device_type == SubGhzRadioDeviceTypeInternal) ? "INT" : "EXT");
+    canvas_invert_color(canvas);
+}
+
+static void
+    subghz_read_raw_draw_saved_center_action(Canvas* canvas, const char* label, bool filled) {
+    canvas_set_font(canvas, FontSecondary);
+    const uint16_t icon_width = icon_get_width(&I_ButtonCenter_7x7);
+    const uint16_t content_width = icon_width + 4U + canvas_string_width(canvas, label);
+    uint16_t button_width = content_width + 8U;
+    if(button_width < 38U) button_width = 38U;
+
+    const int32_t button_x = (128 - button_width) / 2;
+    const int32_t content_x = button_x + (button_width - content_width) / 2;
+
+    if(filled) {
+        canvas_draw_rbox(canvas, button_x, 52, button_width, 12, 2);
+        canvas_invert_color(canvas);
+    } else {
+        canvas_draw_rframe(canvas, button_x, 52, button_width, 12, 2);
+    }
+
+    canvas_draw_icon(canvas, content_x, 55, &I_ButtonCenter_7x7);
+    canvas_draw_str(canvas, content_x + icon_width + 4U, 61, label);
+
+    if(filled) canvas_invert_color(canvas);
+}
+
+static void subghz_read_raw_draw_saved_actions(Canvas* canvas, bool raw_send_only) {
+    canvas_set_font(canvas, FontSecondary);
+
+    if(!raw_send_only) {
+        canvas_draw_icon(canvas, 3, 55, &I_ButtonLeft_4x7);
+        canvas_draw_str(canvas, 11, 62, "New");
+
+        const uint16_t more_width = canvas_string_width(canvas, "More");
+        canvas_draw_str(canvas, 117 - more_width, 62, "More");
+        canvas_draw_icon(canvas, 121, 55, &I_ButtonRight_4x7);
+    }
+
+    subghz_read_raw_draw_saved_center_action(canvas, "Send", true);
+}
+
+static bool subghz_read_raw_is_transmitting(SubGhzReadRAWStatus status) {
+    return (status == SubGhzReadRAWStatusTX) || (status == SubGhzReadRAWStatusTXRepeat) ||
+           (status == SubGhzReadRAWStatusLoadKeyTX) ||
+           (status == SubGhzReadRAWStatusLoadKeyTXRepeat);
+}
+
+static void subghz_read_raw_draw_live_header(Canvas* canvas, SubGhzReadRAWModel* model) {
     canvas_set_font(canvas, FontSecondary);
     canvas_draw_str(canvas, 0, 7, furi_string_get_cstr(model->frequency_str));
     canvas_draw_str(canvas, 35, 7, furi_string_get_cstr(model->preset_str));
 
     if(model->not_showing_samples) {
-        // TODO
         canvas_draw_str(
             canvas,
             77,
@@ -313,6 +437,22 @@ void subghz_read_raw_draw(Canvas* canvas, SubGhzReadRAWModel* model) {
     canvas_draw_line(canvas, 0, 14, 115, 14);
     canvas_draw_line(canvas, 0, 48, 115, 48);
     canvas_draw_line(canvas, 115, 14, 115, 48);
+}
+
+void subghz_read_raw_draw(Canvas* canvas, SubGhzReadRAWModel* model) {
+    const bool is_saved_file = !furi_string_empty(model->file_name);
+    const bool is_transmitting = subghz_read_raw_is_transmitting(model->status);
+
+    canvas_set_color(canvas, ColorBlack);
+
+    if(is_saved_file) {
+        subghz_read_raw_draw_saved_header(canvas, model);
+        if(!is_transmitting) {
+            subghz_read_raw_draw_saved_details(canvas, model);
+        }
+    } else {
+        subghz_read_raw_draw_live_header(canvas, model);
+    }
 
     switch(model->status) {
     case SubGhzReadRAWStatusIDLE:
@@ -321,29 +461,26 @@ void subghz_read_raw_draw(Canvas* canvas, SubGhzReadRAWModel* model) {
         elements_button_right(canvas, "Save");
         break;
     case SubGhzReadRAWStatusLoadKeyIDLE:
-        if(!model->raw_send_only) {
-            elements_button_left(canvas, "New");
-            elements_button_right(canvas, "More");
+        if(is_saved_file) {
+            subghz_read_raw_draw_saved_actions(canvas, model->raw_send_only);
+        } else {
+            if(!model->raw_send_only) {
+                elements_button_left(canvas, "New");
+                elements_button_right(canvas, "More");
+            }
+            elements_button_center(canvas, "Send");
         }
-        elements_button_center(canvas, "Send");
-        elements_text_box(
-            canvas,
-            4,
-            20,
-            110,
-            30,
-            AlignCenter,
-            AlignCenter,
-            furi_string_get_cstr(model->file_name),
-            true);
         break;
 
     case SubGhzReadRAWStatusTX:
     case SubGhzReadRAWStatusTXRepeat:
     case SubGhzReadRAWStatusLoadKeyTX:
     case SubGhzReadRAWStatusLoadKeyTXRepeat:
-        graphics_mode = 0;
-        elements_button_center(canvas, "Hold to repeat");
+        if(is_saved_file) {
+            subghz_read_raw_draw_saved_center_action(canvas, "Hold to repeat", false);
+        } else {
+            elements_button_center(canvas, "Hold to repeat");
+        }
         break;
 
     case SubGhzReadRAWStatusStart:
@@ -356,9 +493,9 @@ void subghz_read_raw_draw(Canvas* canvas, SubGhzReadRAWModel* model) {
         break;
     }
 
-    if(graphics_mode == 0) {
+    if(is_transmitting) {
         subghz_read_raw_draw_sin(canvas, model);
-    } else {
+    } else if(!is_saved_file) {
         subghz_read_raw_draw_rssi(canvas, model);
         subghz_read_raw_draw_scale(canvas, model);
         subghz_read_raw_draw_threshold_rssi(canvas, model);
