76f1fa4e35d724a8b4b33887f6b2c16a126ba83f
squazaryu <tumowuh@internet.ru>
2026-08-31 18:58:48 +0300

    chore(release): prepare t-dev-008-010

--- fbt_options.py
+++ fbt_options.py
@@ -16,7 +16,7 @@ DEBUG = 0
 
 # Suffix to add to files when building distribution
 # If OS environment has DIST_SUFFIX set, it will be used instead
-DIST_SUFFIX = "t-dev-008-009"
+DIST_SUFFIX = "t-dev-008-010"
 
 # Post-update slideshow shown by the updater package
 UPDATE_SPLASH = "tumoflip_update"
